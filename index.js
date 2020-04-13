45ae1315d35c2dd25e1dc6b349749598919d063e
Xiaokeaiya <892765860@qq.com>
2020-04-13 15:51:22 +0800

    model3

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 var body = document.body
-for (var i=0;i<100;i++){
+for (var i=0;i<75;i++){
     var img = new Image();
     num = parseInt(Math.random()*273);
     img.src = "pkq/" + num + ".jpg";
     var w = document.body.clientWidth - 120;
     var h = document.body.clientHeight + 650;
-    var x = Math.random()*w;  
-    var y = Math.random()*h;  
+    var x = i%14 * 102 + Math.random()*100;  
+    var y = i%6 * 112 + Math.random()*80;
+    // var x = Math.random()*w;  
+    // var y = Math.random()*h;  
     img.style.left = x + "px";  
     img.style.top = y + "px";
     img.style.width = "100px";
     img.style.hight = "100px";
-    img.style.opacity = Math.random();
+    var a = 0
+    while (a < 0.3){
+        a = Math.random();
+    }
+    img.style.opacity = a;
 
     body.appendChild(img)
 }
